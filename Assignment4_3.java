4869ccd7622bfec47ae731031c2db268deed5b93
Adam Kim <akim456@gmail.com>
2018-07-26 21:27:21 -0400

    FINISHED 5_4_3

---
+++ Assignment4_3.java
@@ -0,0 +1,78 @@
+/*	WEEK 5 ASSIGNMENT 4 - FILES
+	3.	STORING A STUDENT
+
+	Create a class named Student which contains student information.
+	Specifically, create three variables: name, grade, and gpa.
+
+	Create an instance of type Student and read the values from user input.
+	Create three files on your desktop named: names, grades, and gpas.
+	
+	Store the information from this Student in those files.
+	(name stored in names, grade => grades, gpa => gpas)
+*/
+
+
+//	---------------------------------------------------------------------------------------------------
+
+
+package week5_A4_FILES;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Assignment4_3 {
+
+		public static void main(String[] args) throws IOException {
+
+//	INITIALIZE STUDENT CLASS
+			Student1 student = new Student1();
+			
+//	FILE CONSTRUCTORS		
+			File nameFile = new File("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
+																+ "\\src\\week5_A4_FILES\\FILE_BANK\\4_3_FILES\\name.txt");	
+			
+			File gradeFile = new File("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
+																+ "\\src\\week5_A4_FILES\\FILE_BANK\\4_3_FILES\\grade.txt");	
+			
+			File gpaFile = new File("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
+																+ "\\src\\week5_A4_FILES\\FILE_BANK\\4_3_FILES\\gpa.txt");
+			
+//	INITIALIZE ALL WRITER INSTANCES IN THE FILE LOCATIONS		
+			FileWriter nameWriter = new FileWriter(nameFile, true);
+			FileWriter gradeWriter = new FileWriter(gradeFile, true);
+			FileWriter gpaWriter = new FileWriter(gpaFile, true);
+			
+//	WELCOME MSG		
+			Scanner reader = new Scanner(System.in);
+			System.out.println("I will write the student's name, grade, and GPA in 3 seperate files.");
+			
+//	GETS NAME, SETS VALUE TO STUDENT INSTANCE, THEN WRITES VALUE INTO FILE			
+			System.out.println("What is the student's name ?");	
+			String name = reader.nextLine();
+			student.setName(name);
+			nameWriter.write(student.getName());
+		
+//	GETS GRADE, SETS VALUE TO STUDENT INSTANCE, THEN WRITES VALUE INTO FILE			
+			System.out.println("What is the student's grade?");
+			String grade = reader.nextLine();
+			student.setGrade(grade);
+			gradeWriter.write(student.getGrade());
+		
+//	GETS GPA, SETS VALUE TO STUDENT INSTANCE, THEN WRITES VALUE INTO FILE
+			System.out.println("What is the student's GPA?");
+			double gpa = reader.nextDouble();
+			student.setGpa(gpa);
+			gpaWriter.write(Double.toString(student.getGpa()));
+			
+//	CLOSES CONSOLE READER
+			reader.close();
+//	CLOSES ALL WRITERS
+			nameWriter.close();
+			gradeWriter.close();
+			gpaWriter.close();
+			}
+	}
+
+	
